07bc7205e127706416a0357a82958c0e8d6dca00
woodall <dave@woodalls.me>
2014-08-09 13:46:55 -0600

    make it classy

--- ffffound.rb
+++ ffffound.rb
@@ -1,49 +1,71 @@
 require 'launchy'
 
-file = `curl 'http://feeds.feedburner.com/ffffound/everyone'`
-item_string = file[830..-42]
-item_array = item_string.split("media:content url=")[1..-1]
-
-# Break into methods
-# Make sure it pulls right content
-def ext(item)
-  if item.include? 'jpg'
-    'jpg'
-  elsif item.include? 'png'
-    'png'
-  elsif item.include? 'gif'
-    'gif'
-  else
-    ''
+class FFFFound
+  attr_accessor :file, :item_string, :item_array, :images
+  
+  def initialize
+    @images = []
+    get_feed
+    clean_feed
+    coerce_feed
+    compile_images
+    make_file
+    puts 'FFFFetched!'
   end
-end
 
-images = []
-  item_array.each do |item|
-    short_item = item[0..200]
-    end_string = item.index(ext(short_item)) + 2
-    file = item[1..end_string]
-    img = "<img src=\"" + file + "\" >"
-    images << img
+  def get_feed
+    @file = `curl 'http://feeds.feedburner.com/ffffound/everyone'`
+  end
+
+  def clean_feed
+    @item_string = @file[830..-42]
+  end
+
+  def coerce_feed
+    @item_array = @item_string.split("media:content url=")[1..-1]
+  end
+
+  def ext(item)
+    if item.include? 'jpg'
+      'jpg'
+    elsif item.include? 'png'
+      'png'
+    elsif item.include? 'gif'
+      'gif'
+    else
+      ''
+    end
+  end
+
+  def compile_images
+    @item_array.each do |item|
+      short_item = item[0..200]
+      end_string = item.index(ext(short_item)) + 2
+      _file = item[1..end_string]
+      img = "<img src=\"" + _file + "\" >"
+      @images << img
+    end
   end
   
-file_name = Time.now.strftime("%F.html")
-File.open(file_name, 'w+') do |file|
-  file.write "<html><head>"
-  file.write "<link rel='stylesheet' href='style.css'>"
-  file.write "<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>"
-  file.write "<script src='style.js'></script>"
-  file.write "</head>"
+  def make_file  
+    file_name = Time.now.strftime("%F.html")
+    File.open(file_name, 'w+') do |file|
+      file.write "<html><head>"
+      file.write "<link rel='stylesheet' href='style.css'>"
+      file.write "<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>"
+      file.write "<script src='style.js'></script>"
+      file.write "</head>"
 
-  file.puts ""
-  images.each do |i|
-    file.write i
-    file.puts ""
+      file.puts ""
+      images.each do |i|
+        file.write i
+        file.puts ""
+      end
+      file.puts ""
+      file.write "</body></html>"
+    end
+    Launchy.open(Dir.pwd + "/" + file_name)
   end
-  file.puts ""
-  file.write "</body></html>"
 end
 
-puts 'FFFFetched!'
-Launchy.open(Dir.pwd + "/" + file_name)
-
+FFFFound.new
