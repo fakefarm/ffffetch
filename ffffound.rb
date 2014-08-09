b015613674a380edc2eb2ba5ab9169f24aed0cdd
woodall <dave@woodalls.me>
2014-08-09 11:45:56 -0600

    first pass

--- ffffound.rb
+++ ffffound.rb
@@ -1,16 +1,49 @@
-file = `curl 'http://feeds.feedburner.com/ffffound/everyone'`
+require 'launchy'
 
-items = file.split("<media:content url=").drop(1) 
+file = `curl 'http://feeds.feedburner.com/ffffound/everyone'`
+item_string = file[830..-42]
+item_array = item_string.split("media:content url=")[1..-1]
+
+# Break into methods
+# Make sure it pulls right content
+def ext(item)
+  if item.include? 'jpg'
+    'jpg'
+  elsif item.include? 'png'
+    'png'
+  elsif item.include? 'gif'
+    'gif'
+  else
+    ''
+  end
+end
+
 images = []
-  items.each do |item|
-    file = item.split(".jpg")[0][1..-1] 
-    img = "<img src=" + file + ".jpg >"
+  item_array.each do |item|
+    short_item = item[0..200]
+    end_string = item.index(ext(short_item)) + 2
+    file = item[1..end_string]
+    img = "<img src=\"" + file + "\" >"
     images << img
   end
   
-puts images
-File.open('images.html', 'w+') do |file|
+file_name = Time.now.strftime("%F.html")
+File.open(file_name, 'w+') do |file|
+  file.write "<html><head>"
+  file.write "<link rel='stylesheet' href='style.css'>"
+  file.write "<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>"
+  file.write "<script src='style.js'></script>"
+  file.write "</head>"
+
+  file.puts ""
   images.each do |i|
     file.write i
+    file.puts ""
   end
+  file.puts ""
+  file.write "</body></html>"
 end
+
+puts 'FFFFetched!'
+Launchy.open(Dir.pwd + "/" + file_name)
+
